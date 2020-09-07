8d1aaa847f82118fb854198517b9a73095a5cb84
Navilan <navilan@puthir.in>
2020-09-07 12:14:46 +0530

    Create the project

---
+++ src/Urai.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Navilan
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Navilan <navilan@folds.in>
+
+A static website generator with focus on structured writing
+-}
+
+module Urai
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
